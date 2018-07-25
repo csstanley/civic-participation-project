ba54016db5fdeaca5e103ffed8644a13ac2b7308
Christopher Stanley <cssthewriter@gmail.com>
2018-07-24 20:21:38 -0400

    something

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("client/build"));
 
 app.get('/*', function(req, res) {
   console.log(__dirname);
-  res.sendFile(path.join(__dirname + 'index.html'))
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 // Start the server //
