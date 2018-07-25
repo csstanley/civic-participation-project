7d73c904004951391dba911a7f54034c6a2b53e2
Christopher Stanley <cssthewriter@gmail.com>
2018-07-25 01:05:18 -0400

    navbar component

--- client/src/components/page-comps/navigation/nav.js
+++ client/src/components/page-comps/navigation/nav.js
@@ -1,22 +1,23 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// export class Navigation extends Component {
-
-//     render() {
-//     return (
-//         <div>
-//             <nav>
-//                 <div class="nav-wrapper">
-//                     <a href="#!" class="brand-logo">Logo</a>
-//                     <ul class="right hide-on-med-and-down">
-//                         <li><a class="waves-effect waves-light btn">Button</a></li>
-//                         <li><a class="waves-effect waves-light btn">Button <i class="material-icons right">cloud</i></a></li>
-//                         <li><a class="waves-effect waves-light btn-large">Large Button</a></li>
-//                     </ul>
-//                 </div>
-//             </nav>
-//         </div>
-//     )
-// }
-
-// }
+export class Navbar extends Component {
+    render () {
+        return (
+            <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#home">Civic Engagement Project</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Link
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link
+              </NavItem>
+            </Nav>
+          </Navbar>
+        )
+    }
+}
